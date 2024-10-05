d401f0ed4993b9a69ba88f84e57463d08badabf5
motomoto111 <cyokotomoto@gmail.com>
2024-10-05 12:18:51 +0900

    Finish static pages2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root   "static_pages#home"
+  get    '/home',    to: 'static_pages#home'    # 追加（必要に応じて）
   get    "/help",    to: "static_pages#help"
   get    "/about",   to: "static_pages#about"
   get    "/contact", to: "static_pages#contact"
